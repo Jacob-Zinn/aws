4336e1489fe54ef62bfc02cb611337806c2ee068
Jacob Zinn <jacobpzinn@gmail.com>
2022-02-16 12:07:35 -0700

    added animation to main page

--- jacobpzinn-web/give/js/anim.js
+++ jacobpzinn-web/give/js/anim.js
@@ -1,42 +1,106 @@
+startWordTranslationAnimation();
 
-setInterval(changeGiveSuggestion(), 3000);
+async function startWordTranslationAnimation() {
+  try {
+    const wordArray = await getNewSuggestion();
+    console.log(`wordArray incoming`);
+    console.log(typeof wordArray);
+    setInterval(() => {changeGiveSuggestion(wordArray)}, 4500);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 
-function changeGiveSuggestion() {
-    let date = new Date();
-    console.log(date.toLocaleTimeString());
-    const give = document.getElementById(`give-suggestion`);
+function changeGiveSuggestion(wordArray) {
+  const giveContainer = document.getElementById(`give-suggestion-container`);
+  const element = document.getElementById(`give-suggestion`);
 
-    let rand = getRandomInt(10);
-    console.log(rand);
+  if (giveContainer.childElementCount >= 2) {
+    console.log(`refraining from adding another quote`);
+    return;
+  }
 
-    const url = `misc/kindWords.txt`
-    fetch(url)
+  let y = 0;
+  let alpha = 1;
+  const yMax = 2; // em
+
+
+  let newElement = document.createElement(`h1`);
+  newElement.classList = `give-suggestion`;
+  newElement.textContent = wordArray[getRandomInt(Object.keys(wordArray).length)].word;
+  newElement.style.top = yMax;
+  newElement.setAttribute(`id`, `give-suggestion`);
+  newElement.addEventListener(`click`, alphaAnim);
+  giveContainer.appendChild(newElement);
+
+  let suggestionInterval = setInterval(() => {
+    if (y >= yMax) {
+      newElement.style.opacity = 1;
+      newElement.style.top = 0;
+      clearInterval(suggestionInterval);
+
+      // making replacing old element
+      giveContainer.removeChild(element);
+    } else {
+      alpha -= 0.05;
+      y += 0.1;
+      element.style.opacity = alpha;
+      element.style.top = `${y}em`;
+
+      newElement.style.opacity = 1 - alpha;
+      newElement.style.top = y - yMax;
+    }
+  }, 25);
+}
+
+
+async function getNewSuggestion() {
+
+  let rand = getRandomInt(10);
+  console.log(rand);
+
+  const url = `misc/kindWords.txt`;
+  return await fetch(url)
     .then((response) => {
       return response.json();
     })
     .then((json) => {
-      console.log(json);
-      console.log(json[0]);
-      console.log("Got " + json[0].city);
-      return json;
-    })
-    
-    
+      return json; // array
+    });
 }
 
 function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
+  return Math.floor(Math.random() * max);
+}
+
+// Alpha anim
+document
+  .getElementById(`give-suggestion`)
   
-const giveContainer = document.getElementById(`give-suggestion-container`);
 
-// let fontSize = giveContainer.style.fontSize;
-// let largerSize = fontSize * 1.1
+  function alphaAnim() {
+    let element = document.getElementById(`give-suggestion`);
 
-// giveContainer.addEventListener(`mouseenter`, (event) => {
-//     give.style.fontSize = largerSize;
-// });
-// giveContainer.addEventListener(`mouseleave`, () => {
-//     give.style.fontSize = fontSize;
-// });
+    getQuote(element);
+
+    let fadingOut = true;
+    element.style.opacity = 0.95;
+
+    let anim = setInterval(() => {
+      let curAlpha = element.style.opacity;
+
+      if (curAlpha <= 0) {
+        fadingOut = false;
+        element.style.opacity = 0;
+        element.style.opacity = curAlpha + 0.05;
+      } else if (curAlpha >= 1) {
+        clearInterval(anim);
+      } else if (fadingOut) {
+        element.style.opacity = parseFloat(curAlpha) - 0.1;
+      } else if (!fadingOut) {
+        element.style.opacity = parseFloat(curAlpha) + 0.07;
+        console.log(`testing`);
+      }
+    }, 10);
+  }
--- jacobpzinn-web/give/js/quotes.js
+++ jacobpzinn-web/give/js/quotes.js
@@ -22,25 +22,28 @@ GET /random?maxLength=50
 GET /random?minLength=100&maxLength=140
 */
 
-let giveQuote = document.getElementById(`give-suggestion`);
-giveQuote.addEventListener(`click`, (event) => {
-  const message = document.getElementById(`message`);
-  console.log(message);
-  console.log(typeof message);
-  let url = api_url
-    .concat(giveQuote.innerText.replace(/[^a-z]/gi, ""))
-    .concat(`|friendship`);
+function getQuote(element) {
+  const api_url = `https://api.quotable.io/random?tags=`;
 
+  const messageElement = document.getElementById(`message`);
+  let url = api_url
+    .concat(element.innerText.replace(/[^a-z]/gi, ""));
+    // .concat(`|friendship`);
+
+  modifyMessage(url, messageElement)
+}
+
+function modifyMessage(url, element) {
   getapi(url)
     .then((quote) => {
       message.innerText = quote[`content`];
     })
     .catch((error) => {
+      const messageElement = document.getElementById(`message`);
+      modifyMessage(`https://api.quotable.io/random`, messageElement)
       console.log(`Error: ${error.message}`);
     });
-});
-
-const api_url = `https://api.quotable.io/random?tags=`;
+}
 
 async function getapi(url) {
   const response = await fetch(url);
@@ -50,31 +53,4 @@ async function getapi(url) {
   } else {
     throw Error(response.statusText);
   }
-  //   console.log(data[`content`]);
 }
-
-// const express = require('express');
-// const request = require('request');
-
-// const app = express();
-
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   next();
-// });
-
-// app.get('/jokes/random', (req, res) => {
-//   request(
-//     { url: 'https://cors-anywhere.herokuapp.com/https://joke-api-strict-cors.appspot.com/jokes/random' },
-//     (error, response, body) => {
-//       if (error || response.statusCode !== 200) {
-//         return res.status(500).json({ type: 'error', message: err.message });
-//       }
-
-//       res.json(JSON.parse(body));
-//     }
-//   )
-// });
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => console.log(`listening on ${PORT}`));
