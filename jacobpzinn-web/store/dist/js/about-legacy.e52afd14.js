c21c296cb176fdb76afd5c750f88b5231228da21
Jacob Zinn <jacobpzinn@gmail.com>
2022-03-04 10:33:39 -0700

    created store pwd :wq

---
+++ jacobpzinn-web/store/dist/js/about-legacy.e52afd14.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkgrocery_store"]=self["webpackChunkgrocery_store"]||[]).push([[443],{5830:function(e,t,r){r.r(t),r.d(t,{default:function(){return l}});var n=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},s=[function(){var e=this,t=e.$createElement,r=e._self._c||t;return r("div",{staticClass:"about"},[r("h1",[e._v("This is an about page")])])}],u=r(1001),a={},c=(0,u.Z)(a,n,s,!1,null,null,null),l=c.exports}}]);
+//# sourceMappingURL=about-legacy.e52afd14.js.map
